3b54aca767d8a411885f4cb3a7273df2cd0093fe
Дана <154999200+DanaMilo@users.noreply.github.com>
2025-07-19 11:39:14 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-pt1g=l$+@jviu(1gt4d2a^4!aqggcw4dd*b)5+0o%@(_*lvnu8'
+
+# Application definition
+
+INSTALLED_APPS = [
+    "papka",
+]
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+USE_TZ = False
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
